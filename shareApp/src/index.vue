c17207beb70923e20d4b375c0b968330d27df48c
xuesk <xuesk@midea.com>
2019-03-06 20:19:06 +0800

    修改文案

--- shareApp/src/index.vue
+++ shareApp/src/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <image :src="logo" class="logo" />
-    <text class="greeting">The environment is ready!</text>
+    <text class="greeting">ok</text>
     <router-view/>
   </div>
 </template>
