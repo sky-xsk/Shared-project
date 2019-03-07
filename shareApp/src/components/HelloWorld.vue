48a0e80a40c024454a52827d06a46defcefda159
xuesk <xuesk@midea.com>
2019-03-07 11:36:00 +0800

    edits

--- shareApp/src/components/HelloWorld.vue
+++ shareApp/src/components/HelloWorld.vue
@@ -1,3 +1,17 @@
 <template>
   <text class="message">Now, let's use Vue.js to build your Weex app.</text>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
+}
+</script>
