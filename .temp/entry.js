2a20da81ea569617166bc7a92ab55961cc429566
xsk-sky <2530485432@qq.com>
2019-05-08 11:39:32 +0800

    add

---
+++ .temp/entry.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import weex from 'weex-vue-render'
+/*global Vue*/
+weex.init(Vue)
+/* weex initialized here, please do not move this line */
+const { router } = require('./router');
+const App = require('@/index.vue');
+import mixins from '@/mixins/index';
+import * as filters from '@/filters/index';
+// 注册全局过滤器
+Object.keys(filters).forEach(key => {
+    Vue.filter(key, filters[key]);
+})
+Vue.mixin(mixins);
+/* eslint-disable no-new */
+new Vue(Vue.util.extend({el: '#root', router}, App));
+router.push('/');
+
---
+++ .temp/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+/*global Vue*/
+import Router from 'vue-router'
+import pages from '@/views/pages'
+Vue.use(Router)
+
+export const router = new Router({
+  mode: 'abstract',
+  routes: [
+    {
+      path: '/',
+      name: 'pages',
+      component: pages,
+    },
+  ]
+})
